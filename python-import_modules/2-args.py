fd3438b509591477206531c1e7de74c78b5777ed
Sierra <SierraHunt@protonmail.com>
2026-05-10 12:48:26 +0000

    File for task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("1 argument:")
+    print("1: {0}".format(sys.argv[1]))
+else:
+    print("{0} arguments:".format(len(sys.argv) - 1))
+    j = 0
+    for i in sys.argv:
+        if j > 0:
+            print("{0}: {1}".format(j, sys.argv[j]))
+        j = j + 1
