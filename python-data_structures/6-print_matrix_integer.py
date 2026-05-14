09c1914e905afa19465ab2a912df712b5e3702f1
Sierra <SierraHunt@protonmail.com>
2026-05-14 02:11:38 +0000

    Revert last commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[]):
-    if len(matrix) = 0:
-        print("")
-        return
     for i in matrix:
         for j in range(0, len(i)):
             if j < (len(i) - 1):
