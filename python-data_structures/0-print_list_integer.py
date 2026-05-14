01a5bf7b0390a83c07c350142538e37f3b67a24a
Sierra <SierraHunt@protonmail.com>
2026-05-14 01:11:57 +0000

    Init directory and file for task 0

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_list_integer(mylist=[]):
+    for i in mylist:
+        print("{0}".format(i))
---
+++ python-data_structures/README.md
@@ -0,0 +1,2 @@
+# Data Structures
+In this directory I am learning about data structures in Python!
