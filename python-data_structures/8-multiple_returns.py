22fa0ae62a1e490e21c5dad156200fb0492bf01d
Sierra <SierraHunt@protonmail.com>
2026-05-14 02:45:03 +0000

    Change output of no input

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        fchar = 0
+        fchar = "None"
     else:
         fchar = sentence[0]
     return (len(sentence), fchar)
