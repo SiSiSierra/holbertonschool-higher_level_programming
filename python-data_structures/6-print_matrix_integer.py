45620127c217d50f0e2cd47f760be0555eb94447
Sierra <SierraHunt@protonmail.com>
2026-05-14 02:07:50 +0000

    Added print newline with no input

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[]):
+    if len(matrix) = 0:
+        print("")
+        return
     for i in matrix:
         for j in range(0, len(i)):
             if j < (len(i) - 1):
