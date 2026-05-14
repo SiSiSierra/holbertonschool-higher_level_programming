d2756484cb488ab7cda2c4af2ac9cd40d81e0cb7
Sierra <SierraHunt@protonmail.com>
2026-05-14 02:03:55 +0000

    File for task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[]):
+    for i in matrix:
+        for j in range(0, len(i)):
+            if j < (len(i) - 1):
+                suffix = ' '
+            else:
+                suffix = '\n'
+            print("{0}".format(i[j]), end = suffix)
+
