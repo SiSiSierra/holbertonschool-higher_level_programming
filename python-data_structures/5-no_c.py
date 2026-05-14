9619a5cda31081373647c39afee250ac5026609a
Sierra <SierraHunt@protonmail.com>
2026-05-14 01:54:15 +0000

    Fix early return

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = list(my_string)
+    c = 0
+    C = 0
     for i in range(0, len(new_string)):
-        if new_string[i] == 'c' or new_string[i] == "C":
-            new_string.pop(i)
-            return "".join(new_string)
+        if new_string[i] == 'c':
+            c += 1
+        elif new_string[i] == "C":
+            C += 1
+    for i in range(0, c):
+        new_string.remove("c")
+    for i in range(0, C):
+        new_string.remove("C")
+    return "".join(new_string)
