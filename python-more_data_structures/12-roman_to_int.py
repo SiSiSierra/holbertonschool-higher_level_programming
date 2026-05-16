a4e1e0ed80dddadb4d14d9c7b3b3c231933a7577
Sierra <SierraHunt@protonmail.com>
2026-05-16 19:25:33 +1000

    Fix oversight in styling adjustments

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     numerals = {"I": 1, "V": 5, "X": 10, "L": 50,
                 "C": 100, "D": 500, "M": 1000}
-    if type(roman_string) is str:
+    if type(roman_string) is not str:
         return (0)
     number = 0
     skip = False
