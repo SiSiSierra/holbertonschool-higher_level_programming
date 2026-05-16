f710eb9312c3ce3cc9bfaa70aa693a57c649bd1a
Sierra <SierraHunt@protonmail.com>
2026-05-16 19:17:43 +1000

    File for task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if type(roman_string) != str:
+        return (0)
+    number = 0
+    skip = False
+    for letter in range(len(roman_string)):
+        if skip:
+            skip = False
+            continue
+        if letter + 1 < len(roman_string):
+            if numerals[roman_string[letter]] < numerals[roman_string[letter + 1]]:
+                skip = True
+                number += numerals[roman_string[letter + 1]] - numerals[roman_string[letter]]
+                continue
+        number += numerals[roman_string[letter]]
+    return (number)
