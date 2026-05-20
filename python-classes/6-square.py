93cf704698c1c6e277b68a8ea4b1b1824628c416
Sierra <SierraHunt@protonmail.com>
2026-05-20 12:08:19 +1000

    Additional docs

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -18,15 +18,31 @@ class Square():
         my_print(): Print the square as hashes
     """
     def __init__(self, size=0, position=(0, 0)):
+        """Intialises the Square class with size and position
+
+        Parameters:
+            size: Int indicating width on all sides
+            position: Tuple of ints indicating offset in stdout
+
+        Returns: The new square object
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """ Gets the size attribute
+
+        Returns: __size
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """ Sets the size attribute with an integer
+
+        Returns: nothing
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -35,10 +51,18 @@ class Square():
 
     @property
     def position(self):
+        """ Gets the position attribute
+
+        Returns: __position
+        """
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        """ Sets the position attribute with a given tuple
+
+        Returns: nothing
+        """
         if type(value) is not tuple or len(value) < 2\
             or type(value[0]) is not int or type(value[1]) is not int\
                 or min(value) < 0:
