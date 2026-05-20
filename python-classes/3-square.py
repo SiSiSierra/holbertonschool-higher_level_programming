3f20e0e5c6f36e534b495d11cff42c0631f153bd
Sierra <SierraHunt@protonmail.com>
2026-05-20 10:27:00 +1000

    File for task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module that defines a class
+
+Classes:
+    Square(): Defines a square class
+"""
+
+
+class Square():
+    """ Square class
+
+    Attributes:
+        private __size: Size of width and height
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        return (self.__size ** 2)
