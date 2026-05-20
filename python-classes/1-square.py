2b7a15e6dd2a76fe5ae3e22208e20c385e1834b5
Sierra <SierraHunt@protonmail.com>
2026-05-20 10:20:24 +1000

    File for task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module that defines a class
+
+Classes:
+    Square(): Defines a square class
+"""
+
+
+class Square():
+    """ Square class
+
+    Attributes:
+        private __size: Size of width and height
+    """
+    def __init__(self, size):
+        self.__size = size
+    pass
