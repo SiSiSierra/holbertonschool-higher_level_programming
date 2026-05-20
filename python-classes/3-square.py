5f7c17bc5c2c19961508a319d4aac888e49a757c
Sierra <SierraHunt@protonmail.com>
2026-05-20 10:29:36 +1000

    Style fixes and documentation

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,6 +11,10 @@ class Square():
 
     Attributes:
         private __size: Size of width and height
+
+    Functions:
+        public area(): Return area of square
+
     """
     def __init__(self, size=0):
         if type(size) is not int:
@@ -18,6 +22,6 @@ class Square():
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         return (self.__size ** 2)
