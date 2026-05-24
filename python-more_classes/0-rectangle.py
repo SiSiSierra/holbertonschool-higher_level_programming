983da12b541c5e47ec3b1d8fdeef6cd1c79b420e
Sierra <SierraHunt@protonmail.com>
2026-05-24 18:27:55 +1000

    Init project dir and files for tasks 0-3

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module defining a rectangle class
+
+Classes:
+    Rectangle: Defines a rectangle
+"""
+
+
+class Rectangle():
+    """Rectangle class
+
+    Attributes:
+
+    Functions:
+    """
+    pass
