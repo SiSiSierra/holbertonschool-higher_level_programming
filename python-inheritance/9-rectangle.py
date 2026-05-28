7cb8b6b77b58e1c21a68741cbebda1c60358da76
Sierra <SierraHunt@protonmail.com>
2026-05-29 00:23:29 +1000

    File for task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module
+
+Classes:
+    Rectangle(BaseGeometry)
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class defining a shape
+
+    Inherits: BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        print(self.str())
+    
+    def area(self):
+        """Defines the area of the rectangle
+
+        Returns: width * height to get area
+        """
+        return self.__width * self.__height
