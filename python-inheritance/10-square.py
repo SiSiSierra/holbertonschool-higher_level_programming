bfd57d400f54b943f46f99434392560686698b6c
Sierra <SierraHunt@protonmail.com>
2026-05-29 00:37:17 +1000

    Adjusted int validation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -14,6 +14,7 @@ class Square(Rectangle):
         area(self)
     """
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
 
     def area(self):
