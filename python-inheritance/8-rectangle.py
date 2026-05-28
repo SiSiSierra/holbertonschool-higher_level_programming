4d36ccf0c23b4f7a7bc5cb302681d750f6c3f8ca
Sierra <SierraHunt@protonmail.com>
2026-05-29 00:15:05 +1000

    File for task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module
+
+Classes:
+    Rectangle(BaseGeometry)
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class defining a shape
+
+    Inherits: BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
