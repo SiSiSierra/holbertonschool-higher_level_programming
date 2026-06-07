24b1dec992d77d56844e4a85568be1e2923d800a
Sierra <SierraHunt@protonmail.com>
2026-06-07 18:04:35 +1000

    Added documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Main run file"""
 import json, sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 def main():
+    """ Add runtime args to json file
+
+    Returns: nothing
+    """
     if len(sys.argv) == 1:
         args = []
     else:
