43fd2141891326a8eeef5c5e08b0ec621c6245dd
Sierra <SierraHunt@protonmail.com>
2026-06-07 18:02:26 +1000

    File for task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json, sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+def main():
+    if len(sys.argv) == 1:
+        args = []
+    else:
+        args = sys.argv[1:]
+    old_args = load_from_json_file("add_item.json")
+    old_args.extend(args)
+    save_to_json_file(old_args, "add_item.json")
+    return
+
+if __name__ == "__main__":
+    main()
