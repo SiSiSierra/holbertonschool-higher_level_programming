c31fe66e874f9cf76163ab63b6adc65fb4b41c35
Sierra <SierraHunt@protonmail.com>
2026-06-28 23:35:38 +1000

    File for task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to list all states from db
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.sql import select
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    conn = engine.connect()
+    result = conn.execute(select([State]).order_by("id"))
+    i = 0
+    for row in result:
+        i += 1
+        print("{}: {}".format(i, row[1]))
