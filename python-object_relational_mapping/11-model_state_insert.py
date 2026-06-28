4dae41b0baa735689c8b521a5abd6591d9471545
Sierra <SierraHunt@protonmail.com>
2026-06-29 02:51:44 +1000

    File for task 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to list all states from db
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.sql import insert
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisiana = State(name='Louisiana')
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
