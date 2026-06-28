d94f3e9a24811806ceae51dfd012a7047c8ff6b6
Sierra <SierraHunt@protonmail.com>
2026-06-28 21:52:26 +1000

    File for task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module to test connecting to the local database
+
+Functions
+    main()
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """ main program function
+    """
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY name = (\"{}\") \
+ORDER BY id;".format(sys.argv[4]))
+    m = cursor.fetchall()
+    for state in m:
+        print(state)
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
