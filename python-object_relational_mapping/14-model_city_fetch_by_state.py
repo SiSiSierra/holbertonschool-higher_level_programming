8f43e3ff4fc11db8c939b1e832212f608f721cea
Sierra <SierraHunt@protonmail.com>
2026-07-01 12:48:35 +1000

    ...

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,7 +3,6 @@
 """
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.sql import select
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
@@ -13,7 +12,8 @@ if __name__ == "__main__":
             'mysql+mysqldb://{}:{}@localhost:3306/{}'
             .format(sys.argv[1], sys.argv[2], sys.argv[3]),
             pool_pre_ping=True)
-    Base.metadata.create_all(engine)    
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
     
@@ -23,5 +23,4 @@ if __name__ == "__main__":
 
     for state, city in result:
         print(f"{state.name}: ({city.id}) {city.name}")
-
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,7 +5,6 @@ Classes:
     City(Base)
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 from model_state import Base
 
 class City(Base):
@@ -17,5 +16,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    #state = relationship("State")
