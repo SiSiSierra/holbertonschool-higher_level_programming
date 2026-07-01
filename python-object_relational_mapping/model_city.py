7f591c998796a2e6d520cc5832442bbd2f698287
Sierra <SierraHunt@protonmail.com>
2026-07-01 12:34:13 +1000

    Removed relationship declariation

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -13,8 +13,9 @@ class City(Base):
     """
 
     __tablename__ = "cities"
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
-    state = relationship("State")
+    #state = relationship("State")
