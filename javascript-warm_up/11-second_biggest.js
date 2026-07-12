a13a2d47f13df3bff4423b410cc9e9ee5f5fab37
Sierra <SierraHunt@protonmail.com>
2026-07-13 01:04:57 +1000

    add semi

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (isNaN(process.argv[2]) || isNaN(process.argv[3])) {
   process.exit(0);
 }
 const nums = process.argv.slice(2, process.argv.length);
-nums.sort(function (a, b) { return a - b });
+nums.sort(function (a, b) { return a - b; });
 nums.reverse();
 console.log(nums[1]);
