a2ba1ad7c607e28e47ded746faffd42e01f44432
Sierra <SierraHunt@protonmail.com>
2026-07-12 23:44:13 +1000

    Change to isNaN

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (typeof (Number(process.argv[2])) === 'number') {
+if (!isNaN(process.argv[2])) {
   console.log('My number: ' + Number(process.argv[2]));
 } else {
   console.log('Not a number');
